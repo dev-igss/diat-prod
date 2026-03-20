c838e9afdb7233cf9fd4a1d07701f645cb99756a
dev-igss <devigsshgq@gmail.com>
2026-03-20 10:57:50 -0600

    cambios pdf solicitudes

--- code/code-dev/resources/views/admin/diet_request/print_lote.blade.php
+++ code/code-dev/resources/views/admin/diet_request/print_lote.blade.php
@@ -7,7 +7,6 @@ ini_set('max_execution_time', 0); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title> Solicitud de Dietas </title>
 
     <style>
 
