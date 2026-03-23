f47d70b05c550bf3b0d8cacb2c7a80d43dbfd650
dev-igss <devigsshgq@gmail.com>
2026-03-23 10:10:52 -0600

    Update print_lote.blade.php

--- code/code-dev/resources/views/admin/diet_request/print_lote.blade.php
+++ code/code-dev/resources/views/admin/diet_request/print_lote.blade.php
@@ -7,6 +7,7 @@ ini_set('max_execution_time', 0); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title> Solicitud de Dietas </title>
 
     <style>
 
@@ -75,7 +76,7 @@ ini_set('max_execution_time', 0); ?>
                     </tr>
 
                     <TR>
-                        <TH ROWSPAN="4" style="width: 150px;">LÍQUIDAS</TH>
+                        <TH ROWSPAN="4" style="width: 150px;">Líquidas</TH>
                         <TH ROWSPAN="2" style="width: 150px;">Claros</TH>
                         <TD colspan="2"  style="width: 100px;">
                             @foreach($dr->details as $d)
@@ -1724,7 +1725,7 @@ ini_set('max_execution_time', 0); ?>
 
                     <TR>
                         <TH ROWSPAN="4">DIABÉTICA</TH>
-                        <TH> 1,500 Calorías</TH>
+                        <TH> 1,500 CalorÃ­as</TH>
                         <TD colspan="2">
                             @foreach($dr->details as $d)
                                 @if($d->iddiet == "8" && $d->iddiet_request == $dr->id)
@@ -1796,7 +1797,7 @@ ini_set('max_execution_time', 0); ?>
                     </TR>
 
                     <TR>
-                        <TH> 1,800 Calorías</TH>
+                        <TH> 1,800 CalorÃ­as</TH>
                         <TD colspan="2">
                             @foreach($dr->details as $d)
                                 @if($d->iddiet == "9" && $d->iddiet_request == $dr->id)
@@ -5105,14 +5106,7 @@ ini_set('max_execution_time', 0); ?>
                                 @endif
                             @endforeach
                         </TD>
-                    </TR>
-
- 
-
- 
-
-
-                    
+                    </TR>            
 
                     
 
