04725d73079da38e7e2132016dacd94829747a9d
dev-igss <devigsshgq@gmail.com>
2026-09-22 17:47:01 -0600

    Update print_bs.blade.php

--- code/code-dev/resources/views/admin/diet_request/print_bs.blade.php
+++ code/code-dev/resources/views/admin/diet_request/print_bs.blade.php
@@ -1,1 +1,48 @@
-vale de banco de sangre
+<!DOCTYPE>
+<html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+<style>
+
+    table {
+        border-collapse: collapse;
+    }
+
+    table,
+    th,
+    td {
+        border: 1px solid black;
+    }
+
+    th,
+    td {
+        padding: 5px;
+    }
+    .page-break {
+        page-break-after: always;
+    }
+</style>
+
+<body style="font-size: 14px; font-family: 'Roboto Slab', serif;">
+    <div style="float: left; margin-top: -25px;" >
+        <img src="{{ url('img/Isotipo.png') }}" alt="" width="50" height="50">
+    </div>
+
+
+        <div style="text-align: center; margin-top: -25px;">
+            <span><strong> INSTITUTO GUATEMALTECO DE SEGURIDAD SOCIAL </strong> </span>
+            <span> Banco de Sangre </span>
+        </div>
+        <div style="text-align: center; margin-top: -25px;">
+            <span><strong> SOLICITUD DE REFRIGERIO PARA </strong> </span>
+            <span><strong> DONADORES DEL BANCO DE SANGRE </strong> </span>
+        </div>
+
+        <div style="float: right; margin-top: 0px;">
+            <span><strong> SPS-1071 </strong> </span>
+        </div>
+
+</body>
